8f8622803f52654933c32b422c5ff10b4dc93291
Will Bamberg <wbamberg@mozilla.com>
2019-05-08 15:42:02 -0700

    Remove logging

--- scripts/package/package-contributors.js
+++ scripts/package/package-contributors.js
@@ -5,8 +5,6 @@ const marked = require('marked');
 
 function package(contributorsPath) {
     const contributorsMD = fs.readFileSync(contributorsPath, 'utf8');
-    console.log(contributorsMD)
-        console.log(marked(contributorsMD))
     return marked(contributorsMD);
 }
 
