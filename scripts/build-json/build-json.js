2e2faca43c6d4c31dcba12987a9e65ba6ceb1b81
Will Bamberg <wbamberg@mozilla.com>
2019-05-13 16:19:12 -0700

    First commit of code to build multiple pages

--- scripts/build-json/build-json.js
+++ scripts/build-json/build-json.js
@@ -2,59 +2,38 @@ const fs = require('fs');
 const path = require('path');
 const yaml = require('js-yaml');
 
-const bcd = require('./resolve-bcd');
-const examples = require('./compose-examples');
-const attributes = require('./compose-attributes');
-const prose = require('./slice-prose');
-const contributors = require('./resolve-contributors');
+const buildPage = require('./build-page-json');
 
-const htmlElements = '/content/html/elements';
-
-const writeToFile = (propertyName, json) => {
-  const data = {
-    html: {
-      elements: {
-        [propertyName]: json,
-      }
+function walk(directory, filepaths) {
+    const files = fs.readdirSync(directory);
+    for (let filename of files) {
+        const filepath = path.join(directory, filename);
+        if (filename === 'meta.yaml') {
+            filepaths.push(directory);
+            continue;
+        }
+        if (fs.statSync(filepath).isDirectory()) {
+            walk(filepath, filepaths);
+        }
     }
-  };
-
-  const dest = path.join(process.cwd(),'packaged/html/elements', `${propertyName}.json`);
-  const destDir = path.dirname(dest);
-  if (!fs.existsSync(destDir)) {
-      fs.mkdirSync(destDir, { recursive: true });
-  }
-  fs.writeFileSync(dest, `${JSON.stringify(data, null, 2)}`);
-};
-
-function package(elementName) {
-    const elementPath = path.join(process.cwd(), htmlElements, elementName);
-
-    if (!fs.existsSync(elementPath)) {
-        console.error(`Could not find an element called "${elementName}"`);
-        return;
-    }
-
-    // open meta.yaml
-    const meta = yaml.safeLoad(fs.readFileSync(path.join(elementPath, 'meta.yaml'), 'utf8'));
-
-    // initialise some paths for more resources
-    const examplesPaths = meta.examples.map(relativePath => path.join(elementPath, relativePath));
-    const attributesPath = path.join(elementPath, meta.attributes['element-specific']);
-    const prosePath = path.join(elementPath, 'prose.md');
-    const contributorsPath = path.join(elementPath, 'contributors.md');
-  
-    // make the package
-    const element = {};
-    element.title = meta.title;
-    element.interactive_example_url = meta['interactive-example'];
-    element.browser_compatibility = bcd.package(meta['browser-compatibility']);
-    element.examples = examples.package(examplesPaths);
-    element.attributes = attributes.package(attributesPath);
-    element.prose = prose.package(prosePath);
-    element.contributors = contributors.package(contributorsPath);
-
-    writeToFile(elementName, element);
 }
 
-package(process.argv[2]);
+function collectItems(directory) {
+    let filepaths = [];
+    walk(directory, filepaths);
+    filepaths = filepaths.map( filepath => filepath.slice(path.join(process.cwd(), './content/').length) )
+    return filepaths;
+}
+
+function buildJSON() {
+    const items = collectItems(path.resolve(process.cwd(), './content'));
+    for (let item of items) {
+        buildPage.buildPageJSON(item);
+    }
+}
+
+if (process.argv[2]) {
+    buildPage.buildPageJSON(process.argv[2]);
+} else {
+    buildJSON();
+}
---
+++ scripts/build-json/build-page-json.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const path = require('path');
+const yaml = require('js-yaml');
+
+const bcd = require('./resolve-bcd');
+const examples = require('./compose-examples');
+const attributes = require('./compose-attributes');
+const prose = require('./slice-prose');
+const contributors = require('./resolve-contributors');
+
+const htmlElements = '/content/html/elements';
+
+const writeToFile = (propertyName, json) => {
+  const data = {
+    html: {
+      elements: {
+        [propertyName]: json,
+      }
+    }
+  };
+
+  const dest = path.join(process.cwd(),'packaged/html/elements', `${propertyName}.json`);
+  const destDir = path.dirname(dest);
+  if (!fs.existsSync(destDir)) {
+      fs.mkdirSync(destDir, { recursive: true });
+  }
+  fs.writeFileSync(dest, `${JSON.stringify(data, null, 2)}`);
+};
+
+function buildPageJSON(elementPath) {
+    if (!elementPath.startsWith('html/elements')) {
+        console.log('Not an HTMl element');
+        return;
+    }
+    elementPath = path.join(process.cwd(), './content', elementPath);
+    const elementName = path.basename(elementPath);
+
+    if (!fs.existsSync(elementPath)) {
+        console.error(`Could not find an element called "${elementName}"`);
+        return;
+    }
+
+    // open meta.yaml
+    const meta = yaml.safeLoad(fs.readFileSync(path.join(elementPath, 'meta.yaml'), 'utf8'));
+
+    // initialise some paths for more resources
+    const examplesPaths = meta.examples.map(relativePath => path.join(elementPath, relativePath));
+    const prosePath = path.join(elementPath, 'prose.md');
+    const contributorsPath = path.join(elementPath, 'contributors.md');
+  
+    // make the package
+    const element = {};
+    element.title = meta.title;
+    element.interactive_example_url = meta['interactive-example'];
+    element.browser_compatibility = bcd.package(meta['browser-compatibility']);
+    if (meta.attributes['element-specific']) {
+        const attributesPath = path.join(elementPath, meta.attributes['element-specific']);
+        element.attributes = attributes.package(attributesPath);
+    } else {
+        element.attributes = null;
+    }
+    element.examples = examples.package(examplesPaths);
+    element.prose = prose.package(prosePath);
+    element.contributors = contributors.package(contributorsPath);
+
+    writeToFile(elementName, element);
+}
+
+module.exports = {
+    buildPageJSON
+}
