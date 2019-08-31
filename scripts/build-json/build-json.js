b97f22a1acca5ed7f42c2c2536c698e4ce8a6fdd
Peter Bengtsson <mail@peterbe.com>
2019-08-31 17:59:53 -0400

    make build-page-json more flexible (#116)
    * make build-page-json more flexible
    
    * remove comment
    
    * feedbacked
    
    * refactored
    
    * path.sep
    
    * filepath not filePaths

--- scripts/build-json/build-json.js
+++ scripts/build-json/build-json.js
@@ -2,41 +2,51 @@ const fs = require('fs');
 const path = require('path');
 
 const buildPage = require('./build-page-json');
+const { ROOT } = require('./constants');
 
-function walk(directory, filepaths) {
+
+function findItems(directory, searchPaths, filepaths = []) {
     const files = fs.readdirSync(directory);
     for (let filename of files) {
         const filepath = path.join(directory, filename);
         if (path.extname(filename) === '.md') {
-            filepaths.push(path.join(directory, filename));
-            continue;
-        }
-        if (fs.statSync(filepath).isDirectory()) {
-            walk(filepath, filepaths);
+            if (!searchPaths.length || searchPaths.some(s => filepath.includes(s))) {
+                filepaths.push(filepath);
+            }
+        } else if (fs.statSync(filepath).isDirectory()) {
+            findItems(filepath, searchPaths, filepaths);
         }
     }
-}
-
-function collectItems(directory, searchPaths) {
-    let filepaths = [];
-    walk(directory, filepaths);
-    filepaths = filepaths.map( 
-        filepath => filepath.slice(path.join(process.cwd(), './content/').length) 
-    ).filter(filepath => !searchPaths.length || searchPaths.some(searchPath => filepath.includes(searchPath)))
     return filepaths;
 }
 
 function buildJSON(searchPaths) {
     let errors = 0;
-    const items = collectItems(path.resolve(process.cwd(), './content'), searchPaths);
+    const items = findItems(path.resolve(ROOT, 'content'), searchPaths);
     if (!items.length && searchPaths.length) {
         console.error("No elements found");
         errors++;
     }
-    for (let item of items) {
-        const parsed = path.parse(item);
-        errors += buildPage.buildPageJSON(parsed.dir, parsed.base);
+
+    const cwd = process.cwd() + path.sep;
+    function printPath(p) {
+        return p.replace(cwd, '');
     }
+    items.forEach(async item => {
+        let built
+        try {
+            built = await buildPage.buildPageJSON(item);
+            const { docsPath, destPath } = built;
+            if (destPath !== null) {
+                console.log(`Packaged ${printPath(docsPath)} to ${printPath(destPath)}`);
+            }
+        } catch (error) {
+            console.warn(`Failed to build page JSON from ${item}`);
+            console.error(error);
+            errors++;
+        }
+
+    })
     return errors;
 }
 
