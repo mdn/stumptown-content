b92c4de3a59534423199f0f14fafb175238e1cbb
Peter Bengtsson <mail@peterbe.com>
2019-09-19 20:28:20 -0400

    Protect against checked in diffs (#140)
    * Protect against checked in diffs
    
    Fixes #139
    
    * oops
    
    * un-oops
    
    * more oops
    
    * try again
    
    * un-oops again
    
    * proper script
    
    * fixing back
    
    * feedbacked

---
+++ scripts/ci/git.js
@@ -0,0 +1,22 @@
+/**
+ * Check that there are no problems with the code from git.
+ */
+const spawn = require("child_process").spawnSync;
+
+function spawnStr(cmd, ...args) {
+    const spawned = spawn(cmd, ...args);
+    return spawned.stdout.toString().trim();
+}
+
+const foundLines = spawnStr("git", ["grep", "-n", ">>>>>>>"])
+    .split(/\n/g)
+    .filter(line => {
+        return !(line.includes(".travis.yml") || line.includes("scripts/ci/"));
+    });
+if (foundLines.length) {
+    console.warn("The following lines look like git diff markers:");
+    for (let line of foundLines) {
+        console.log(line);
+    }
+    process.exitCode = 1;
+}
