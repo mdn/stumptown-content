66932c41ef02916fbf30f38e2f7766c15b7f6531
Will Bamberg <wbamberg@mozilla.com>
2019-10-04 08:46:07 -0700

    Add a comment about regex exec() call

--- scripts/scraper/process-macros/extract-macro-calls.js
+++ scripts/scraper/process-macros/extract-macro-calls.js
@@ -11,6 +11,11 @@ function extractMacroCalls(macroName, html) {
   let allInvocations = [];
   let thisInvocation = [];
 
+  // When the 'g' flag is set for a regex, it stores the index of the last match.
+  // You can then use `exec` to iterate through the string finding successive matches:
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec#Finding_successive_matches
+  // A better way would be to use matchAll (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/matchAll)
+  // but that's only in Node from v12, and we have to support the LTS, which is v10.
   while ((thisInvocation = regex.exec(html)) !== null) {
       // Split by ",", then strip leading and trailing spaces and quotes
       allInvocations.push((thisInvocation[1].split(',').map(piece => piece.replace(/^['" ]+|['" ]+$/g, ''))));
