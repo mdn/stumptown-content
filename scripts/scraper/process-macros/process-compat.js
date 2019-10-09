98e39f1a56049efcf2423fc60d2394d4aada956b
Peter Bengtsson <mail@peterbe.com>
2019-10-09 17:18:35 -0400

    scrape-mdn newlines

--- scripts/scraper/process-macros/process-compat.js
+++ scripts/scraper/process-macros/process-compat.js
@@ -3,8 +3,8 @@ const { removeNode } = require('../clean-html.js');
 
 function processCompat(htmlWithMacroCalls, result) {
   const macroCalls = extractMacroCalls('Compat', htmlWithMacroCalls);
-  if (macroCalls[0]) {
-    result.frontMatter += macroCalls.length? `browser_compatibility: ${macroCalls[0][0]}\n`: '';
+  if (macroCalls.length) {
+    result.frontMatter += `browser_compatibility: ${macroCalls[0][0]}\n`;
   }
   removeNode(result.dom, 'div.bc-data');
   return result;
