d4376ea1b3a41b37710637a4255d6817bb24d600
Daniel D. Beck <daniel@ddbeck.com>
2019-10-16 13:30:22 +0100

    Remove needless check for recipe field in YAML

--- scripts/linter/plugins/valid-recipes.js
+++ scripts/linter/plugins/valid-recipes.js
@@ -2,10 +2,6 @@ const visit = require("unist-util-visit");
 
 const ruleId = "stumptown-linter:frontmatter-valid-recipe";
 
-function hasRecipeInYaml(node) {
-    return node.data && node.data.yaml && node.data.yaml.recipe;
-}
-
 /**
  * If a tree contains a YAML (frontmatter) node and it contains a recipe, then confirm that it's a recognized recipe.
  *
@@ -17,21 +13,19 @@ function attacher(options) {
     return function transformer(tree, file) {
         visit(tree, "yaml", node => {
             const validRecipes = Object.keys(recipes);
-            if (hasRecipeInYaml(node)) {
-                const recipe = node.data.yaml.recipe;
+            const recipe = node.data.yaml.recipe;
 
-                if (validRecipes.includes(recipe)) {
-                    tree.data = tree.data || {};
-                    tree.data.recipe = recipes[recipe];
-                    tree.data.recipeName = recipe;
-                } else {
-                    const m = file.message(
-                        `"${recipe}" recipe was not found in /recipes`,
-                        node,
-                        ruleId
-                    );
-                    m.fatal = true;
-                }
+            if (validRecipes.includes(recipe)) {
+                tree.data = tree.data || {};
+                tree.data.recipe = recipes[recipe];
+                tree.data.recipeName = recipe;
+            } else {
+                const m = file.message(
+                    `"${recipe}" recipe was not found in /recipes`,
+                    node,
+                    ruleId
+                );
+                m.fatal = true;
             }
         });
     };
