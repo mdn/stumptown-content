a6035952aa40688e52183de5223a2f2e81df7eaa
Daniel D. Beck <daniel@ddbeck.com>
2019-10-16 12:51:04 +0100

    Handle unparsable YAML frontmatter

--- scripts/linter/yaml-loader.js
+++ scripts/linter/yaml-loader.js
@@ -5,9 +5,17 @@ const yaml = require("js-yaml");
  * Parse the value of YAML-type unist nodes (i.e., frontmatter) and add the resulting object to the node's `data.yaml` field.
  */
 function attacher() {
-    return function transformer(tree) {
+    return function transformer(tree, file) {
         visit(tree, "yaml", node => {
-            node.data = { yaml: yaml.safeLoad(node.value) };
+            try {
+                node.data = { yaml: yaml.safeLoad(node.value) };
+            } catch (error) {
+                const message = file.message(
+                    `${error.name}: ${error.message}`,
+                    node
+                );
+                message.fatal = true;
+            }
         });
     };
 }
