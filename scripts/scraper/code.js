2c9af59fe3be879bfa04118c97c6c48afc1660f5
Will Bamberg <wbamberg@mozilla.com>
2019-10-18 16:15:12 -0700

    Preserve language information for pre blocks

---
+++ scripts/scraper/code.js
@@ -0,0 +1,34 @@
+'use strict'
+
+module.exports = code
+
+const is = require('hast-util-is-element');
+const has = require('hast-util-has-property');
+const toText = require('hast-util-to-text');
+const trim = require('trim-trailing-lines');
+
+/**
+ * An alternative version of the built-in handler for
+ * converting `pre` blocks from HTML to Markdown.
+ *
+ * The built-in version, in hast-util-to-mdast,
+ * sets `lang` based on classes like "language-html".
+ *
+ * But in MDN code blocks use "brush: html" to indicate
+ * language. So this handler looks for that instead.
+ */
+
+function code(h, node) {
+  let lang;
+
+  if (node.tagName === 'pre') {
+    if (has(node, 'className')) {
+      let brushIndex = node.properties.className.indexOf('brush:');
+      if ((brushIndex !== -1) && (brushIndex < node.properties.className.length-1)) {
+        lang = node.properties.className[brushIndex + 1];
+      }
+    }
+  }
+
+  return h(node, 'code', {lang: lang || null, meta: null}, trim(toText(node)))
+}
--- scripts/scraper/to-markdown.js
+++ scripts/scraper/to-markdown.js
@@ -2,6 +2,7 @@ const unified = require('unified');
 const parse = require('rehype-parse');
 const stringify = require('remark-stringify');
 const rehype2remark = require('rehype-remark');
+const code = require('./code');
 
 /**
  * Converts the HTML -> Markdown using unified.
@@ -9,8 +10,8 @@ const rehype2remark = require('rehype-remark');
 function toMarkdown(html) {
   return unified()
     .use(parse)
-    .use(rehype2remark)
-    .use(stringify)
+    .use(rehype2remark, {handlers: {pre: code}})
+    .use(stringify, {fences: true})
     .process(html);
 }
 
