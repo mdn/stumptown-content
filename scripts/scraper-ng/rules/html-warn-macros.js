22adaf053ac86db6907dc57f9593ac496fb781ad
Daniel D. Beck <daniel@ddbeck.com>
2019-12-17 13:47:35 +0000

    Stop using unified-lint-rule for macro warnings
    unified-lint-rule enforces the use of a single ruleId (it clobbers any
    attempt to set another ruleId), so I had to make this a regular unified
    plugin instead.

--- scripts/scraper-ng/rules/html-warn-macros.js
+++ scripts/scraper-ng/rules/html-warn-macros.js
@@ -1,28 +1,32 @@
-const rule = require("unified-lint-rule");
 const visit = require("unist-util-visit-parents");
 
 /**
  * Issue a warning for each macro call (except for the array of macro names in
  * `allowedMacros`)
  */
-function warnOnMacros(tree, file, allowedMacros) {
+function attacher(allowedMacros) {
   allowedMacros = Array.isArray(allowedMacros) ? allowedMacros : [];
 
-  visit(
-    tree,
-    node =>
-      node.type === "text" &&
-      node.data &&
-      node.data.macroName &&
-      !allowedMacros.includes(node.data.macroName),
-    (node, ancestors) => {
-      // Because macro nodes are generated, they don't have position
-      // information. The parent node's position is used instead.
-      const parent = ancestors[ancestors.length - 1];
-      const message = file.message(`Macro: ${node.data.macroName}`, parent);
-      message.note = `With arguments: ${JSON.stringify(node.data.macroParams)}`;
-    }
-  );
+  return function warnOnMacros(tree, file) {
+    visit(
+      tree,
+      node =>
+        node.type === "text" &&
+        node.data &&
+        node.data.macroName &&
+        !allowedMacros.includes(node.data.macroName),
+      (node, ancestors) => {
+        // Because macro nodes are generated, they don't have position
+        // information. The parent node's position is used instead.
+        const parent = ancestors[ancestors.length - 1];
+        const message = file.message(`Macro: ${node.data.macroName}`, parent);
+        message.ruleId = `html-warn-on-macros:${node.data.macroName}`;
+        message.note = `With arguments: ${JSON.stringify(
+          node.data.macroParams
+        )}`;
+      }
+    );
+  };
 }
 
-module.exports = rule("html-warn-on-macros", warnOnMacros);
+module.exports = attacher;
