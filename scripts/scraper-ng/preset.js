58bf204681205325c8c299facff0c0a9303f48bd
Daniel D. Beck <daniel@ddbeck.com>
2020-02-25 18:04:22 +0000

    Drop generic macro call requirement

--- scripts/scraper-ng/preset.js
+++ scripts/scraper-ng/preset.js
@@ -1,5 +1,4 @@
 const allowedMacros = ["Compat"];
-const requiredMacros = ["Compat"];
 
 module.exports = {
   settings: {},
@@ -11,7 +10,6 @@ module.exports = {
     // For rules that don't need settings of any kind:
     //   [require('./rules/rule-name')]
     [require("./rules/file-require-recipe")],
-    [require("./rules/html-require-macros"), ["error", requiredMacros]],
     [require("./rules/html-warn-macros"), allowedMacros],
     [require("./rules/html-require-recipe-ingredients")]
   ]
