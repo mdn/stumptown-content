90165ae482e9397ddf37b7b8dfbb31562b93cdaf
Daniel D. Beck <daniel@ddbeck.com>
2020-03-03 19:14:12 +0000

    Fix bug a bug when printing a summary with no errors (#340)
    * Improve output for scraping with no errors
    
    Don't throw an exception when there aren't any errors.
    
    * Increase cheer

--- scripts/scraper-ng/vfile-reporter-summary.js
+++ scripts/scraper-ng/vfile-reporter-summary.js
@@ -31,6 +31,14 @@ function reporter(files) {
 function summarize(files) {
   const count = countRules(files);
 
+  if (Object.entries(count).length === 0) {
+    return [
+      "",
+      chalk.yellow(chalk.underline("Summary")),
+      "No issues found. Hooray! 🎉"
+    ].join("\n");
+  }
+
   const sortedRules = Object.entries(count)
     .map(([ruleId, details]) => ({
       ruleId,
