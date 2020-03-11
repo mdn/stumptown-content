39af29c549ade9b7fe5ab016e714a956ed83d680
Will Bamberg <wbamberg@mozilla.com>
2020-03-11 09:08:33 -0700

    Simplify checking BCD properties

--- scripts/build-json/resolve-status.js
+++ scripts/build-json/resolve-status.js
@@ -1,9 +1,9 @@
 function packageStatus(specifications, bcd) {
-  const bcdHasStatus = !!bcd.__compat && !!bcd.__compat.status;
   return {
     non_standard: specifications === "non-standard",
-    experimental: bcdHasStatus && bcd.__compat.status.experimental,
-    deprecated: bcdHasStatus && bcd.__compat.status.deprecated
+    experimental:
+      bcd.__compat !== undefined && bcd.__compat.status.experimental,
+    deprecated: bcd.__compat !== undefined && bcd.__compat.status.deprecated
   };
 }
 
