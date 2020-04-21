f098f036726175d39e4a946053ad7eb67f8ec2da
Daniel D. Beck <daniel@ddbeck.com>
2020-04-21 18:38:37 +0100

    Test all required ingredients trigger a message

---
+++ scripts/scraper-ng/test/html-require-recipe-ingredients.test.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const fs = require("fs");
+
+const { processFromSource } = require("./utils");
+
+require("./jest.setup.js");
+
+const recipesDir = path.resolve(__dirname, "../../../recipes");
+const allRecipes = fs
+  .readdirSync(recipesDir)
+  .map((f) => path.join(recipesDir, f));
+
+describe("html-require-recipe-ingredients", () => {
+  test.only.each(allRecipes)(
+    "All recipe ingredients are loggable: %s",
+    (recipePath) => {
+      const file = processFromSource("", recipePath);
+      const ingredients = file.data.recipe.body;
+      const required = ingredients.filter(
+        (i) => !(i.endsWith("?") || i.endsWith("*"))
+      );
+
+      expect(file.messages.length).toBeGreaterThanOrEqual(required.length);
+      expect(file).not.hasMessageWithId("prose.*");
+      for (const ingredient of required) {
+        expect(file).hasMessageWithId(ingredient);
+      }
+    }
+  );
+});
