c9083a25bbbde833ae8ae899f86acea727737993
Daniel D. Beck <daniel@ddbeck.com>
2020-05-05 15:49:10 +0100

    Add ingredient order tests to describe group

--- scripts/scraper-ng/test/html-require-ingredient-order.test.js
+++ scripts/scraper-ng/test/html-require-ingredient-order.test.js
@@ -2,49 +2,51 @@ const path = require("path");
 
 const { process } = require("./framework/utils");
 
-test("ingredient order", () => {
-  const file = process(
-    path.resolve(__dirname, "./javascript-constructor.html"),
-    "javascript-constructor"
-  );
+describe("html-require-ingredient-order", () => {
+  test("ingredient order", () => {
+    const file = process(
+      path.resolve(__dirname, "./javascript-constructor.html"),
+      "javascript-constructor"
+    );
 
-  expect(file).not.hasMessageWithId("ingredient-out-of-order");
+    expect(file).not.hasMessageWithId("ingredient-out-of-order");
+  });
+
+  test("missing optional ingredient is OK", () => {
+    const file = process(
+      `<h2 id="Syntax">Syntax</h2>
+       <h2 id="Error_type">Error type</h2>`,
+      {
+        body: ["prose.syntax", "data.static_methods?", "prose.error_type"],
+      }
+    );
+
+    expect(file).not.hasMessageWithId("ingredient-out-of-order");
+  });
+
+  test("ingredient disorder", () => {
+    const file = process(
+      `<h2 id="Error_type">Error type</h2>
+       <h2 id="Something_else">Something else</h2>
+       <h2 id="Description">Description</h2>`,
+      {
+        body: ["prose.description", "prose.*", "prose.error_type"],
+      }
+    );
+
+    expect(file).hasMessageWithId("ingredient-out-of-order");
+  });
+
+  // test("prose.* ingredient disorder", () => {
+  //   const file = process(
+  //     `<h2 id="Description">Description</h2>
+  //      <h2 id="Error_type">Error type</h2>
+  //      <h2 id="Something_else">Something else</h2>`,
+  //     {
+  //       body: ["prose.description", "prose.*", "prose.error_type"],
+  //     }
+  //   );
+
+  //   expect(file).hasMessageWithId("ingredient-out-of-order");
+  // });
 });
-
-test("missing optional ingredient is OK", () => {
-  const file = process(
-    `<h2 id="Syntax">Syntax</h2>
-     <h2 id="Error_type">Error type</h2>`,
-    {
-      body: ["prose.syntax", "data.static_methods?", "prose.error_type"],
-    }
-  );
-
-  expect(file).not.hasMessageWithId("ingredient-out-of-order");
-});
-
-test("ingredient disorder", () => {
-  const file = process(
-    `<h2 id="Error_type">Error type</h2>
-     <h2 id="Something_else">Something else</h2>
-     <h2 id="Description">Description</h2>`,
-    {
-      body: ["prose.description", "prose.*", "prose.error_type"],
-    }
-  );
-
-  expect(file).hasMessageWithId("ingredient-out-of-order");
-});
-
-// test("prose.* ingredient disorder", () => {
-//   const file = process(
-//     `<h2 id="Description">Description</h2>
-//      <h2 id="Error_type">Error type</h2>
-//      <h2 id="Something_else">Something else</h2>`,
-//     {
-//       body: ["prose.description", "prose.*", "prose.error_type"],
-//     }
-//   );
-
-//   expect(file).hasMessageWithId("ingredient-out-of-order");
-// });
