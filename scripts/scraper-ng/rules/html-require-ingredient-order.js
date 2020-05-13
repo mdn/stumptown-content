400115dcb9939a6472cceb29ce8e0f8120ce1a3c
Daniel D. Beck <daniel@ddbeck.com>
2020-05-13 17:20:57 +0100

    Clean up comments and logging

--- scripts/scraper-ng/rules/html-require-ingredient-order.js
+++ scripts/scraper-ng/rules/html-require-ingredient-order.js
@@ -6,33 +6,28 @@ const source = "html-require-ingredient-order";
 
 /**
  * A plugin that sets an error when an ingredient appears before or after another ingredient.
+ *
  */
 function attacher() {
   return function lintIngredientOrder(tree, file) {
-    // TODO: I decided to treat all H2s not identified as another ingredient as a `prose.*` ingredient
     file.data.ingredients.push(...collectProseStar(tree, file));
 
-    // Collect all the page ingredients actually found on the page (the set of known + prose.*, excluding missing)
+    // Collect all page ingredients with positions
     const pageIngredients = file.data.ingredients
       .filter((i) => i.position !== null)
       .sort((a, b) => a.position.data.pageIndex - b.position.data.pageIndex); // Sorted by their order on the page
-    const pageIngredientNames = new Set(pageIngredients.map((i) => i.name));
 
     // Get the sequence of expected recipe ingredients, excluding those that
     // definitely don't appear in the page. This skips both 1) optional
     // ingredients that were omitted (which is permissible) and 2) required
-    // ingredients that weren't found
+    // ingredients that are missing (which is out-of-scope for this rule)
+    const pageIngredientNames = new Set(pageIngredients.map((i) => i.name));
     const recipeIngredientNames = file.data.recipe.body.filter((i) =>
       pageIngredientNames.has(i)
     );
 
-    // TODO: Filter the names of expected recipe ingredients by the list of known ingredient names
-    // TODO: Pop r1 and i1 and compare. Are they the same name?
-    // TODO: If r is prose.* and i is prose.* --> continue as long as i is prose.*, then pop r
-    // TODO: elif r !== i --> error, then pop
-    // TODO: continue until empty
-
-    // In the list of expected ingredients, replace `prose.*` with a sufficient number of `prose.*` entries to cover all such ingredients
+    // Pad the sequence of expected ingredients with additioanl `prose.*`
+    // entries to cover all such ingredients
     if (recipeIngredientNames.includes("prose.*")) {
       const padCount = pageIngredients.reduce(
         (sum, ingredient) => sum + (ingredient.name == "prose.*" ? 1 : 0),
@@ -45,18 +40,6 @@ function attacher() {
       );
     }
 
-    const output = [];
-    for (
-      let index = 0;
-      index < Math.max(recipeIngredientNames.length, pageIngredients.length);
-      index++
-    ) {
-      const recipe = recipeIngredientNames[index];
-      const ingredient = pageIngredients[index].name;
-      output.push([recipe, ingredient]);
-    }
-    console.log(output);
-
     for (
       let index = 0;
       index < Math.max(recipeIngredientNames.length, pageIngredients.length);
@@ -66,9 +49,6 @@ function attacher() {
       const currentIngredient = pageIngredients[index];
 
       if (currentIngredient.name !== expectedIngredientName) {
-        console.log(
-          `Logging ${expectedIngredientName}, ${currentIngredient.name}`
-        );
         const message = file.message(
           `${currentIngredient.name} not expected in this order`,
           currentIngredient.position,
