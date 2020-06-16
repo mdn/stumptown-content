54b1bd73fcf317420a375f6a333c2c86a5670327
Daniel D. Beck <daniel@ddbeck.com>
2020-06-16 14:48:39 +0100

    Implement data.formal_definition handler

--- scripts/scraper-ng/rules/html-require-recipe-ingredients/ingredient-handlers/data-formal-definition.js
+++ scripts/scraper-ng/rules/html-require-recipe-ingredients/ingredient-handlers/data-formal-definition.js
@@ -1,7 +1,76 @@
+const { select } = require("hast-util-select");
+const visit = require("unist-util-visit");
+
+const { isMacro, isWhiteSpaceTextNode, sliceSection } = require("./utils");
+
+const isCSSInfo = (node) => isMacro(node, "CSSInfo");
+
 function handleDataFormalDefinition(tree, logger) {
   const id = "Formal_definition";
   const body = select(`body`, tree);
   const heading = select(`h2#${id}`, body);
+
+  if (heading === null) {
+    logger.expected(body, `h2#${id}`, "expected-heading");
+    return null;
+  }
+
+  // Section must have an immediate child P element containing the CSSInfo macro
+  const section = sliceSection(heading, body);
+
+  // Find the first P with a CSSInfo macro as one of its children
+  let expectedMacro;
+  let expectedP = section.children.find((node) => {
+    if (node.tagName !== "p" || !node.children) {
+      return false;
+    }
+
+    // Get first CSSInfo child node, if it exists
+    expectedMacro = node.children.find(isCSSInfo);
+    if (expectedMacro !== undefined) {
+      return true;
+    }
+
+    return false;
+  });
+
+  for (const exp of [expectedMacro, expectedP]) {
+    if (!exp) {
+      logger.expected(exp, "CSSInfo macro paragraph", "expected-cssinfo-macro");
+      return null;
+    }
+  }
+
+  // After the H2, the section must only contain `<p>{{CSSInfo}}</p>` (and
+  // space)
+  const isExpected = (node) =>
+    node.type === "root" ||
+    isWhiteSpaceTextNode(node) ||
+    node === expectedP ||
+    node === expectedMacro;
+
+  let extraneousNode = null;
+  visit(section, (node) => {
+    if (node === heading) {
+      return visit.SKIP;
+    } else if (isExpected(node)) {
+      return visit.CONTINUE;
+    } else {
+      extraneousNode = node;
+      return visit.EXIT;
+    }
+  });
+
+  if (extraneousNode !== null) {
+    logger.fail(
+      extraneousNode,
+      "No other elements allowed in formal definition",
+      "expected-macro-only"
+    );
+    return null;
+  }
+
+  return heading;
 }
 
 module.exports = handleDataFormalDefinition;
