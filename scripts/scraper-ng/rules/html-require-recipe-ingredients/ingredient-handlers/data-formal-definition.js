597811175787601a8655cc9b600a1915c0567769
Daniel D. Beck <daniel@ddbeck.com>
2020-06-23 15:04:42 +0100

    Move section handler for data.formal_defintion into arrow function

--- scripts/scraper-ng/rules/html-require-recipe-ingredients/ingredient-handlers/data-formal-definition.js
+++ scripts/scraper-ng/rules/html-require-recipe-ingredients/ingredient-handlers/data-formal-definition.js
@@ -1,48 +1,49 @@
 const { findUnexpectedNode, isMacro, sectionHandler } = require("./utils");
 
-const handleDataFormalDefinition = sectionHandler("Formal_definition", handle);
+const handleDataFormalDefinition = sectionHandler(
+  "Formal_definition",
+  (section, logger) => {
+    const heading = section.children[0];
 
-function handle(section, logger) {
-  const heading = section.children[0];
-
-  // Find the first P with a CSSInfo macro as one of its children
-  let expectedMacro;
-  let expectedP = section.children.find((node) => {
-    if (node.tagName === "p" && node.children) {
-      // Get first CSSInfo child node, if it exists
-      expectedMacro = node.children.find((node) => isMacro(node, "CSSInfo"));
-      if (expectedMacro) {
-        return true;
+    // Find the first P with a CSSInfo macro as one of its children
+    let expectedMacro;
+    let expectedP = section.children.find((node) => {
+      if (node.tagName === "p" && node.children) {
+        // Get first CSSInfo child node, if it exists
+        expectedMacro = node.children.find((node) => isMacro(node, "CSSInfo"));
+        if (expectedMacro) {
+          return true;
+        }
       }
+      return false;
+    });
+
+    if (!expectedP) {
+      logger.expected(
+        section,
+        "CSSInfo macro paragraph",
+        "expected-cssinfo-macro"
+      );
+      return null;
     }
-    return false;
-  });
 
-  if (!expectedP) {
-    logger.expected(
+    // The section must contain only a heading and `<p>{{CSSInfo}}</p>`
+    const extraneousNode = findUnexpectedNode(
       section,
-      "CSSInfo macro paragraph",
-      "expected-cssinfo-macro"
+      [heading],
+      [expectedP, expectedMacro]
     );
-    return null;
-  }
+    if (extraneousNode !== null) {
+      logger.fail(
+        extraneousNode,
+        "No other elements allowed in data.formal_definition",
+        "expected-macro-only"
+      );
+      return null;
+    }
 
-  // The section must contain only a heading and `<p>{{CSSInfo}}</p>`
-  const extraneousNode = findUnexpectedNode(
-    section,
-    [heading],
-    [expectedP, expectedMacro]
-  );
-  if (extraneousNode !== null) {
-    logger.fail(
-      extraneousNode,
-      "No other elements allowed in data.formal_definition",
-      "expected-macro-only"
-    );
-    return null;
+    return heading;
   }
-
-  return heading;
-}
+);
 
 module.exports = handleDataFormalDefinition;
