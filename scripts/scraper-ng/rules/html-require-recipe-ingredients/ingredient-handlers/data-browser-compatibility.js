49fda12af7271091757f4ee3e5309525ae90e79d
Daniel D. Beck <daniel@ddbeck.com>
2020-06-30 16:45:32 +0100

    Refactor data.browser_compatibility

--- scripts/scraper-ng/rules/html-require-recipe-ingredients/ingredient-handlers/data-browser-compatibility.js
+++ scripts/scraper-ng/rules/html-require-recipe-ingredients/ingredient-handlers/data-browser-compatibility.js
@@ -1,4 +1,3 @@
-const { select } = require("hast-util-select");
 const visit = require("unist-util-visit");
 
 const utils = require("./utils.js");
@@ -6,31 +5,26 @@ const utils = require("./utils.js");
 /**
  * Handler for the `data.browser_compatibility` ingredient.
  */
-function handleDataBrowserCompatibility(tree, logger) {
-  const id = "Browser_compatibility";
-  const body = select(`body`, tree);
-  const heading = select(`h2#${id}`, body);
+const handleDataBrowserCompatibility = utils.sectionHandler(
+  "Browser_compatibility",
+  (section, logger) => {
+    let macroCount = 0;
+    visit(
+      section,
+      (node) => utils.isMacro(node, "Compat"),
+      () => {
+        macroCount += 1;
+      }
+    );
 
-  if (heading === null) {
-    logger.expected(body, `h2#${id}`, "expected-heading");
-    return null;
-  }
-
-  let macroCount = 0;
-  visit(
-    utils.sliceSection(heading, body),
-    (node) => utils.isMacro(node, "Compat"),
-    () => {
-      macroCount += 1;
+    if (macroCount !== 1) {
+      const heading = section.children[0];
+      logger.expected(heading, "Compat macro", "expected-macro");
+      return false;
     }
-  );
 
-  if (macroCount !== 1) {
-    logger.expected(body, `Compat macro`, "expected-macro");
-    return null;
+    return true;
   }
-
-  return heading;
-}
+);
 
 module.exports = handleDataBrowserCompatibility;
