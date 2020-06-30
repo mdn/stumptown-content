271dc35cac9a6edd2c451be75420fec817b85e27
Daniel D. Beck <daniel@ddbeck.com>
2020-06-30 16:58:07 +0100

    Refactor data.formal_syntax

--- scripts/scraper-ng/rules/html-require-recipe-ingredients/ingredient-handlers/data-formal-syntax.js
+++ scripts/scraper-ng/rules/html-require-recipe-ingredients/ingredient-handlers/data-formal-syntax.js
@@ -5,60 +5,53 @@ const {
   findUnexpectedNode,
   isMacro,
   isWhiteSpaceTextNode,
-  sliceSection,
+  sectionHandler,
 } = require("./utils");
 
-function handleDataFormalSyntax(tree, logger) {
-  const id = "Formal_syntax";
-  const body = select(`body`, tree);
-  const heading = select(`h2#${id}`, body);
-
-  if (heading === null) {
-    logger.expected(body, `h2#${id}`, "expected-heading");
-    return null;
-  }
-
-  // Section must contain pre.syntaxbox
-  const section = sliceSection(heading, body);
-  const expectedSyntaxBox = select("pre.syntaxbox", section);
-  if (expectedSyntaxBox === null) {
-    logger.expected(section, "pre.syntaxbox", "expected-pre.syntaxbox");
-    return null;
-  }
-
-  // pre.syntaxbox must contain CSSSyntax macro
-  let expectedMacro = null;
-  visit(
-    expectedSyntaxBox,
-    (node) => !isWhiteSpaceTextNode(node),
-    (node) => {
-      if (isMacro(node, "CSSSyntax")) {
-        expectedMacro = node;
-        return visit.EXIT;
-      }
+const handleDataFormalSyntax = sectionHandler(
+  "Formal_syntax",
+  (section, logger) => {
+    // Section must contain pre.syntaxbox
+    const expectedSyntaxBox = select("pre.syntaxbox", section);
+    if (expectedSyntaxBox === null) {
+      logger.expected(section, "pre.syntaxbox", "expected-pre.syntaxbox");
+      return false;
     }
-  );
-  if (expectedMacro === null) {
-    logger.expected(section, "CSSSyntax macro", "expected-macro");
-    return null;
-  }
 
-  // The section must contain only the `h2`, `pre.syntaxbox`, and macro call
-  const extraneousNode = findUnexpectedNode(
-    section,
-    [heading],
-    [expectedSyntaxBox, expectedMacro]
-  );
-  if (extraneousNode !== null) {
-    logger.fail(
-      extraneousNode,
-      "No other elements allowed in data.formal_syntax",
-      "syntax-only"
+    // pre.syntaxbox must contain CSSSyntax macro
+    let expectedMacro = null;
+    visit(
+      expectedSyntaxBox,
+      (node) => !isWhiteSpaceTextNode(node),
+      (node) => {
+        if (isMacro(node, "CSSSyntax")) {
+          expectedMacro = node;
+          return visit.EXIT;
+        }
+      }
     );
-    return null;
-  }
+    if (expectedMacro === null) {
+      logger.expected(section, "CSSSyntax macro", "expected-macro");
+      return false;
+    }
 
-  return heading;
-}
+    // The section must contain only the `h2`, `pre.syntaxbox`, and macro call
+    const extraneousNode = findUnexpectedNode(
+      section,
+      [section.children[0]],
+      [expectedSyntaxBox, expectedMacro]
+    );
+    if (extraneousNode !== null) {
+      logger.fail(
+        extraneousNode,
+        "No other elements allowed in data.formal_syntax",
+        "syntax-only"
+      );
+      return false;
+    }
+
+    return true;
+  }
+);
 
 module.exports = handleDataFormalSyntax;
