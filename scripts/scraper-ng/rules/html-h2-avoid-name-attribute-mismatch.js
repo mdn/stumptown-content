df0874d09d3ec18593169a5ab0d2571e518084de
Daniel D. Beck <daniel@ddbeck.com>
2020-07-22 22:37:09 +0100

    Only flag mismatched name attributes

--- scripts/scraper-ng/rules/html-h2-avoid-name-attribute-mismatch.js
+++ scripts/scraper-ng/rules/html-h2-avoid-name-attribute-mismatch.js
@@ -1,9 +1,10 @@
 const { selectAll } = require("hast-util-select");
+const toText = require("hast-util-to-text");
 
 const source = "html-h2-avoid-name-attribute";
-const id = "h2-name-attr";
+const id = "h2-name-attr-mismatch";
 const reason = (node) =>
-  `H2 has name attribute (h2[name="${node.properties.name}"])`;
+  `H2 has name attribute mismatch (h2[name="${node.properties.name}"])`;
 
 /**
  * Warn about H2s with name attributes, since this can lead to surprises.
@@ -11,9 +12,19 @@ const reason = (node) =>
 function htmlH2AvoidNameAttributePlugin() {
   return function warnOnH2NameAttr(tree, file) {
     for (const node of selectAll("h2[name]", tree)) {
-      file.message(reason(node), node, `${source}:${id}`);
+      if (isMismatch(node)) {
+        file.message(reason(node), node, `${source}:${id}`);
+      }
     }
   };
+}
+
+function isMismatch(node) {
+  return toAttr(toText(node)) !== node.properties.name;
+}
+
+function toAttr(str) {
+  return str.replace(/ /g, "_");
 }
 
 module.exports = htmlH2AvoidNameAttributePlugin;
